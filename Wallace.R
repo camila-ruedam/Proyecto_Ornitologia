3d79451f78c666488af09cf573da5274735b90f8
camila-ruedam <cami.rm02@gmail.com>
2021-05-26 19:00:52 -0500

    Descargar Wallace

--- Wallace.R
+++ Wallace.R
@@ -13,8 +13,21 @@ updateR()
 # https://cran.r-project.org/bin/windows/Rtools/
 
 
+
+### Dos opciones para instalar Wallace
+
+##Wallace primera versión:
 install.packages("wallace")
 library(wallace)
+
+
+##Wallace Versión en desarrollo:
+devtools::install_github("https://github.com/wallaceEcoMod/wallace/tree/biomodelos", dependencies = TRUE)
+# Abrir Wallace
+library(wallace)
+run_wallace()
+
+
 
 install.packages("rgbif")
 install.packages("paleobioDB")
@@ -29,14 +42,15 @@ library(maskRangeR)
 library(changeRangeR)
 1
 
-#Si hay Problemas con changeRangeR:
+#Sólo si hay Problemas con changeRangeR:
 install.packages("devtools")
 devtools::install_github("nathanvan/parallelsugar")
 devtools::install_github("https://github.com/andrepazv/changeRangeR/tree/paths_fix", dependencies= TRUE)
 # Cargar paquete
 library(changeRangeR)
 
-#Descargar ENMeval2.0 - Ecological Niche Models evaluation
+
+#Descargar ENMeval2.0 - Ecological Niche Models evaluation - Sirve para la pestaña de Modelos
 # Más información en
 ## https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/2041-210X.12261
 
@@ -50,8 +64,6 @@ library(ENMeval)
 #### Iniciar Wallace
 library(wallace)
 run_wallace()
-
-
 
 
 
